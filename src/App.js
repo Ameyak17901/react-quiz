d8add498e22a88ded1b2ee9dee0350b149ab287d
Ameya Kulkarni <ameyak2001@gmail.com>
2023-08-14 13:55:43 +0530

    Initial Commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,43 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useReducer } from "react";
+import Header from "./Header";
+import Main from "./Main";
 
-function App() {
+const initialState = {
+  questions: [],
+  // "loading", "ready". "error",
+  status: "loading",
+};
+
+function reducer(state, action) {
+  switch (action.type) {
+    case "dataReceived":
+      return {
+        ...state,
+        status: "ready",
+        questions: action.payload,
+      };
+    default:
+      throw new Error("Unknown Action");
+  }
+}
+
+export default function App() {
+  const [state, dispatch] = useReducer(reducer, initialState);
+
+  useEffect(function () {
+    fetch("http://localhost:8000/questions")
+      .then((res) => res.json())
+      .then((data) => dispatch({ type: "dataReceived", payload: data }))
+      .catch((err) => console.error(err.message));
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Header />
+      <Main className="main">
+        <p>1/15</p>
+        <p>Question?</p>
+      </Main>
     </div>
   );
 }
-
-export default App;
